6ae83df21d8ee31baaaabc5eebab7745052bf205
Erik <erik@dombi.ca>
2022-02-10 13:16:33 -0500

    Fix indentation formatting

--- Chrome-Extension/src/background.ts
+++ Chrome-Extension/src/background.ts
@@ -2,18 +2,18 @@ function openPartial(info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab
   if (info.menuItemId != 'openPartial')
     return;
 
-  chrome.tabs.sendMessage(tab?.id ?? 0, {msg: "getXRayPath"}, { frameId: info.frameId }, data => {
+  chrome.tabs.sendMessage(tab?.id ?? 0, { msg: "getXRayPath" }, { frameId: info.frameId }, data => {
     let clickedElement: string = data.value.el;
     let filePath: string = clickedElement.trim().split(' ')[3];
 
-    if(filePath == undefined)
+    if (filePath == undefined)
       return;
 
     console.log(data.value.tags);
-    
+
     fetch(`http://localhost:6070/open?file=${filePath}&tags=${JSON.stringify(data.value.tags)}`)
       .catch(e => {
-        chrome.tabs.sendMessage(tab?.id ?? 0, {msg: "displayErrorHTML", data: 'Failed to connect to VS Code!\nEnsure that Peeky has been activated'}, {frameId: info.frameId}, () => {});
+        chrome.tabs.sendMessage(tab?.id ?? 0, { msg: "displayErrorHTML", data: 'Failed to connect to VS Code!\nEnsure that Peeky has been activated' }, { frameId: info.frameId }, () => { });
       });
   });
 }
--- VSCode-Extension/src/extension.ts
+++ VSCode-Extension/src/extension.ts
@@ -1,12 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {Express} from 'express';
+import { Express } from 'express';
 import { Uri } from 'vscode';
 import { Server } from 'http';
 import axios from 'axios';
 const express = require("express");
-const app : Express = express();
+const app: Express = express();
 const port = 6070;
 
 let peekyStatusBarItem: vscode.StatusBarItem;
@@ -18,92 +18,92 @@ const stopServerCommandId = 'peeky-xray.stopListener';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	console.log("Peeky loaded!");
-	createHTTPListener();
+  console.log("Peeky loaded!");
+  createHTTPListener();
 
 
-    context.subscriptions.push(vscode.commands.registerCommand(startServerCommandId, () => {
-		axios.get(`http://localhost:${port}/close`).finally(() => {
-			serverInstance = app.listen(port, () => {
-				console.log(`Peeky server started at http://localhost:${port}`);
-				peekyStatusBarItem.text = 'Peeky: Active';
-				peekyStatusBarItem.command = stopServerCommandId;
-			});
-		});
-	}));
+  context.subscriptions.push(vscode.commands.registerCommand(startServerCommandId, () => {
+    axios.get(`http://localhost:${port}/close`).finally(() => {
+      serverInstance = app.listen(port, () => {
+        console.log(`Peeky server started at http://localhost:${port}`);
+        peekyStatusBarItem.text = 'Peeky: Active';
+        peekyStatusBarItem.command = stopServerCommandId;
+      });
+    });
+  }));
 
-	context.subscriptions.push(vscode.commands.registerCommand(stopServerCommandId, () => {
-		serverInstance?.close();
-		peekyStatusBarItem.text = 'Peeky: Inactive';
-		peekyStatusBarItem.command = startServerCommandId;
-		console.log(`Peeky server stopped`);
-	}));
+  context.subscriptions.push(vscode.commands.registerCommand(stopServerCommandId, () => {
+    serverInstance?.close();
+    peekyStatusBarItem.text = 'Peeky: Inactive';
+    peekyStatusBarItem.command = startServerCommandId;
+    console.log(`Peeky server stopped`);
+  }));
 
-	peekyStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 10000);
-	peekyStatusBarItem.text = 'Peeky: Inactive';
-	peekyStatusBarItem.command = startServerCommandId;
-	peekyStatusBarItem.show();
+  peekyStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 10000);
+  peekyStatusBarItem.text = 'Peeky: Inactive';
+  peekyStatusBarItem.command = startServerCommandId;
+  peekyStatusBarItem.show();
 }
 
 function createHTTPListener() {
-	app.use(express.json());
+  app.use(express.json());
 
-	app.get('/open', (req, res) => {
-		const resource : string = req.query.file as string;
-		const uri : Uri = Uri.file(resource);
+  app.get('/open', (req, res) => {
+    const resource: string = req.query.file as string;
+    const uri: Uri = Uri.file(resource);
 
-		vscode.workspace.openTextDocument(uri).then((doc: vscode.TextDocument) => {
-			vscode.window.showTextDocument(doc, 1, false).then((e) => {
-				let tags = JSON.parse(req.query.tags as string);
-				let lineCandidate : vscode.TextLine | null = null;
-				let highestScore : number = 0;
-				for(let i = 0; i < e.document.lineCount; i++) {
-					let line: string = e.document.lineAt(i).text;
-					let score : number = 0;
-					for(let tag of tags) {
-						for(let word of tag.split(' ')) {
-							if(line.toLowerCase().includes(word.toLowerCase())) {
-								score += word.length;
-							}
-						}
-					}
-					if(score > highestScore) {
-						lineCandidate = e.document.lineAt(i);
-						highestScore = score;
-					}
-				}
-				console.log(tags);
-				console.log("lineCandidate");
-				console.log(lineCandidate);
-				if(lineCandidate !== null){
-					e.revealRange(lineCandidate.range, vscode.TextEditorRevealType.InCenter);
-					let styleForRegExp = Object.assign({}, { border: "1px solid yellow" },
-					{
-						overviewRulerLane: vscode.OverviewRulerLane.Right
-					});
-					e.setDecorations(
-						vscode.window.createTextEditorDecorationType(styleForRegExp),
-						new Array<vscode.Range>(new vscode.Range(new vscode.Position(lineCandidate.range.start.line, 0), new vscode.Position(lineCandidate.range.start.line, 0)))
-					);
-				}
-			});
-		});
+    vscode.workspace.openTextDocument(uri).then((doc: vscode.TextDocument) => {
+      vscode.window.showTextDocument(doc, 1, false).then((e) => {
+        let tags = JSON.parse(req.query.tags as string);
+        let lineCandidate: vscode.TextLine | null = null;
+        let highestScore: number = 0;
+        for (let i = 0; i < e.document.lineCount; i++) {
+          let line: string = e.document.lineAt(i).text;
+          let score: number = 0;
+          for (let tag of tags) {
+            for (let word of tag.split(' ')) {
+              if (line.toLowerCase().includes(word.toLowerCase())) {
+                score += word.length;
+              }
+            }
+          }
+          if (score > highestScore) {
+            lineCandidate = e.document.lineAt(i);
+            highestScore = score;
+          }
+        }
+        console.log(tags);
+        console.log("lineCandidate");
+        console.log(lineCandidate);
+        if (lineCandidate !== null) {
+          e.revealRange(lineCandidate.range, vscode.TextEditorRevealType.InCenter);
+          let styleForRegExp = Object.assign({}, { border: "1px solid yellow" },
+            {
+              overviewRulerLane: vscode.OverviewRulerLane.Right
+            });
+          e.setDecorations(
+            vscode.window.createTextEditorDecorationType(styleForRegExp),
+            new Array<vscode.Range>(new vscode.Range(new vscode.Position(lineCandidate.range.start.line, 0), new vscode.Position(lineCandidate.range.start.line, 0)))
+          );
+        }
+      });
+    });
 
-		res.sendStatus(200);
-	});
+    res.sendStatus(200);
+  });
 
-	app.get('/close', (req, res) => {
-		serverInstance?.close();
+  app.get('/close', (req, res) => {
+    serverInstance?.close();
 
-		peekyStatusBarItem.text = 'Peeky: Inactive';
-		peekyStatusBarItem.command = startServerCommandId;
-		console.log(`Peeky server stopped`);
+    peekyStatusBarItem.text = 'Peeky: Inactive';
+    peekyStatusBarItem.command = startServerCommandId;
+    console.log(`Peeky server stopped`);
 
-		vscode.window.showInformationMessage("Disabling Peaky as it was enabled in another window");
+    vscode.window.showInformationMessage("Disabling Peaky as it was enabled in another window");
 
-		res.sendStatus(200);
-	});
+    res.sendStatus(200);
+  });
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
